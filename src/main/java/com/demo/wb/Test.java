285ae38835071f996c029bc996d12d492356ebb2
v_zoupengfei <v_zoupengfei@baidu.com>
2017-03-15 17:03:06 +0800

    init

---
+++ src/main/java/com/demo/wb/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.demo.wb;
+
+/**
+ * Created by v_zoupengfei on 2017/3/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
